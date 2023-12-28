bccc4fd9ccd1fa0eea992162315eedfb8dd4585a
JeongHeumChoi <andyc707@gmail.com>
2023-12-28 22:23:06 +0900

    feat: 스프링으로 전환

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -8,23 +8,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+// 설정 정보
+@Configuration
 public class AppConfig {
 
+    // Spring Container에 등록해준다.
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(getMemberRepository()); // 생성자 주입
     }
 
-    private static MemberRepository getMemberRepository() {
+    @Bean
+    public static MemberRepository getMemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
     }
 
+    @Bean
     public static DiscountPolicy getDiscountPolicy() {
         return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
     }
 
 }
